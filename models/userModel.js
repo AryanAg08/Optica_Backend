923548a545690a431a5353bd6f119d4c4765a98f
kartik <kartikaggarwal2004@gmail.com>
2024-02-22 22:15:21 +0530

    User routes Ticket issue and verification

---
+++ models/userModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+
+const userSchema = new schema({
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+    },
+
+    email: { 
+        type: String,
+        required: true,
+        unique: true,
+        min: 6,
+        max: 255,
+
+    },
+    
+    phoneNo: {
+        type: Number,
+        required: true,
+        min: 10,
+    },
+
+    batch: {
+        type: String,
+        required: true,
+    },
+
+    enrollmentNo: {
+        type: String,
+        required: true,
+    },
+
+    branch: {
+        type: String,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 1024,
+    },
+
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+
+});
+
+
+module.exports = mongoose.model('User', userSchema);
