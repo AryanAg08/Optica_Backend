6a30b7d140fd7e3e95f87e3639510296cbd92d4b
AryanAg08 <aryangoyal90@outlook.com>
2024-04-14 14:36:30 +0530

    Adding Limiter

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ app.use(
     })
 );
 app.use (cookieParser(process.env.SECRET));
-app.use(express.json());
+// app.use(express.json());
+app.use(bodyParser.json({ limit: "2mb" }))
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
